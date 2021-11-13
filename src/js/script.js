95084b10e944fecce0f4be058cba096d686519bd
Khalil Asif <58604863+devkhalil1337@users.noreply.github.com>
2021-11-13 15:40:34 +0500

    datetime picker and first and last digit field added

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,56 @@
 
- JsBarcode("#barcode", "98760218018426061121990");
- let today = new Date();
- _time = setZero(today.getHours()) + ":" + setZero(today.getMinutes())+ ":" + setZero(today.getSeconds())
- $("#time").text(_time)
- _date =   today.getDate() + "/" + (today.getMonth()+1) +"/"+today.getFullYear()
- $("#date").text(_date)
+
+$( document ).ready(function() {
+	$('#datetimepicker1').datetimepicker();
+	JsBarcode("#barcode", "98760218018426061121990");
+	let today = new Date();
+	
+	_time = setZero(today.getHours()) + ":" + setZero(today.getMinutes()) + ":" + setZero(today.getSeconds())
+	$("#time").text(_time)
+	_date = today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear()
+	$("#date").text(_date)
+	$("#staticDigits").val("987602180"); //demo purpose
+	
+	
+	$("#generate").submit(function(event) {
+		// alert( "Handler for .submit() called." );
+		let amount = $("#amount").val()		
+		let description = $("#description").val();
+		if(description && description.length > 0)
+			$(".description").text(description);
+		// if(amount && amount > 0){
+		//     $("#amountLogo").text("a2")
+		// }
+		 let couponDate = new Date($("#datetimepicker1").val());
 
 
-$("#generate").submit(function(event){
-    // alert( "Handler for .submit() called." );
-    let amount = $("#amount").val()
-    // if(amount && amount > 0){
-    //     $("#amountLogo").text("a2")
-    // }
-    const staticNumbers = "987602180";
-    today = new Date();
-    let date =  today.getHours() +""+ today.getMinutes() +""+  today.getSeconds() +""+  today.getFullYear()+""+ (today.getMonth()+1)+""+ today.getDate();
 
-    _time = setZero(today.getHours()) + ":" + setZero(today.getMinutes())+ ":" + setZero(today.getSeconds())
-    $("#time").text(_time)
-    _date =   today.getDate() + "/" + (today.getMonth()+1) +"/"+today.getFullYear()
-    $("#date").text(_date)
-    JsBarcode("#barcode", staticNumbers + date + amount);
-    event.preventDefault();
-    $("#generate").trigger('reset');
-    
-})
+		const staticNumbers =  $("#staticDigits").val(); ;
 
 
-function setZero(n){
-    return n < 10 ? "0"+n: n;
-}
+
+		today = new Date();
+
+		let date = couponDate.getHours() + "" + couponDate.getMinutes() + "" + couponDate.getSeconds() + "" + couponDate.getFullYear() + "" + (couponDate.getMonth() + 1) + "" + couponDate.getDate();
+
+		_time = setZero(today.getHours()) + ":" + setZero(today.getMinutes()) + ":" + setZero(today.getSeconds())
+
+
+		$("#time").text(_time)
+		_date = today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear()
+
+		$("#date").text(_date)
+		JsBarcode("#barcode", staticNumbers + date + amount);
+
+
+
+
+		event.preventDefault();
+		// $("#generate").trigger('reset');
+	})
+	
+	function setZero(n) {
+		return n < 10 ? "0" + n : n;
+	}
+});
+
